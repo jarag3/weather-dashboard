2ea66d55486c10117925f760135bf5f04dde4e5a
Jarl Grande <jarl.grande@gmail.com>
2022-04-27 23:26:27 -0700

    fixed button in html, and testing API call

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+
+//     $("#search-button").on("click", function() {
+//         var searchValue = $("#search-value").val();
+//         $("#search-value").val("");
+//         searchWeather(searchValue);
+//     })
+// });
+
+// test API call
+var APIKey = "e80c86cddae3e0f811424e1f7df7c059"
+var currentWeather = [];
+var excludeAlerts = 'Alerts';
+
+var testWeatherAPI = function() {
+    currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=38.581573&lon=-121.494400&appid=e80c86cddae3e0f811424e1f7df7c059`;
+    console.log(currentWeather);
+};
+
+testWeatherAPI();
