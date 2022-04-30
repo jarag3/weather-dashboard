4080cc8eaedcc07a361ca6a337b34a547e5b2602
Jarl Grande <jarl.grande@gmail.com>
2022-04-29 17:29:28 -0700

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,15 @@
-// $(document).ready(function() {
 
-//     $("#search-button").on("click", function() {
-//         var searchValue = $("#search-value").val();
-//         $("#search-value").val("");
-//         searchWeather(searchValue);
-//     })
-// });
 
 // test API call
-var APIKey = "e80c86cddae3e0f811424e1f7df7c059"
-var currentWeather = [];
-var excludeAlerts = 'Alerts';
+// var APIKey = "e80c86cddae3e0f811424e1f7df7c059"
+// var currentWeather = [];
+// var excludeAlerts = 'Alerts';
+// var cityLat;
+// var cityLong;
 
-var testWeatherAPI = function() {
-    currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=38.581573&lon=-121.494400&appid=e80c86cddae3e0f811424e1f7df7c059`;
-    console.log(currentWeather);
-};
+// var testWeatherAPI = function() {
+//     currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=38.581573&lon=-121.494400&appid=e80c86cddae3e0f811424e1f7df7c059`;
+//     console.log(currentWeather);
+// };
 
-testWeatherAPI();
+// testWeatherAPI();
