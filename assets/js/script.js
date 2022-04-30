b9c17149d7b91117fa8572deba1f95c867cf65c2
Jarl Grande <jarl.grande@gmail.com>
2022-04-29 18:08:51 -0700

    var declarations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
+// Variable Declarations
+var searchHistoryList = $('#search-history-list');
+var searchCityInput = $("#search-city");
+var searchCityButton = $("#search-city-button");
+var clearHistoryButton = $("#clear-history");
+
+// Get access to the OpenWeather API
+var APIkey = "e80c86cddae3e0f811424e1f7df7c059"
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + APIkey;
+
+// Easy access to data
+var cityList = [];
+
+// Check if search history exists
+initalizeHistory();
+showClear();
+
+// $.ajax({
+        //     url: ,
+        //     method: "GET"
+        // }).then(function(response){
+        // });
+        // Hitting enter while input is focused will trigger
+        // value added to search history
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // test API call
@@ -12,4 +58,3 @@
 //     console.log(currentWeather);
 // };
 
-// testWeatherAPI();
